f2fcbef4e067f6794fbb4be7f7020b5e9ca48958
valor-null <valeriaconcedsantos@gmail.com>
2025-11-20 15:59:14 -0300

    feat(mobile): configurar API para usar backend deployado no Azure

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://10.0.2.2:5163",
+    baseURL: "https://restartai-api-001.azurewebsites.net",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
